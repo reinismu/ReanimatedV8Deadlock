16d3c565d980e9cd2c743714e0dcb938ffbc6383
Reinis Muiznieks <muiznieks.reinis@gmail.com>
2024-07-30 17:43:48 +0300

    Setup V8

--- android/app/src/main/java/com/reanimatedv8deadlock/MainApplication.kt
+++ android/app/src/main/java/com/reanimatedv8deadlock/MainApplication.kt
@@ -6,10 +6,14 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.JavaScriptExecutorFactory
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.modules.systeminfo.AndroidInfoHelpers
 import com.facebook.soloader.SoLoader
+
+import io.csie.kudo.reactnative.v8.executor.V8ExecutorFactory;
 
 class MainApplication : Application(), ReactApplication {
 
@@ -27,6 +31,13 @@ class MainApplication : Application(), ReactApplication {
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        override fun getJavaScriptExecutorFactory(): JavaScriptExecutorFactory =
+            V8ExecutorFactory(
+                applicationContext,
+                packageName,
+                AndroidInfoHelpers.getFriendlyDeviceName(),
+                useDeveloperSupport
+            )
       }
 
   override val reactHost: ReactHost
